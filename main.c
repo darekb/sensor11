ba45b4177732590d035e2f33dbd5c5343dec8147
darekb <kontakt@3wm.pl>
2017-03-22 07:26:12 +0100

    wip

--- main.c
+++ main.c
@@ -15,7 +15,6 @@
 #include "main.h"
 
 
-
 #include "slUart.h"
 
 
@@ -23,44 +22,66 @@
 #include "slBME180Measure.h"
 #include "slI2C.h"
 #include "BME280.h"
+#include "slSPI.h"
 
 #define SENSOR_ID 11
 
 void clearData();
-void nrf24_Start();
+
+void setupInt0();
+
+void goReset();
 
 void waitForStart();
+
 void compareStrings();
+
 uint8_t setBME280Mode();
+
 uint8_t getDataFromBME280();
+
 uint8_t sendVianRF24L01();
 
 uint8_t pipe1[] = {0xF0, 0xF0, 0xF0, 0xF0, 0xE1};
 uint8_t pipe2[] = {0xF0, 0xF0, 0xF0, 0xF0, 0x95};
 uint8_t data[9];
+uint8_t status;
 struct MEASURE BME180measure = {0, 0, 0, 0, 0};
 volatile uint8_t stage;
 volatile uint16_t counter = 0;
 volatile int f_timer = 0;
-volatile uint8_t listenTime = 0;
+volatile uint8_t sendingTryCount = 0;
 const char startStringSensor11[] = {'s', 't', 'a', 'r', 't', '-', 's', '1', '1'};
+uint8_t *arr;
 
 int main(void) {
 
     slUART_SimpleTransmitInit();
     slUART_WriteString("start.\r\n");
     slI2C_Init();
-    nrf24_Start();
     if (BME280_Init(BME280_OS_T_1, BME280_OS_P_1, BME280_OS_H_1, BME280_FILTER_OFF, BME280_MODE_FORCED,
                     BME280_TSB_1000)) {
         slUART_WriteString("BMP280 init error.\r\n");
     } else {
         slUART_WriteString("BMP280 init done.\r\n");
     }
-    stage = 11;
+    slSPI_Init();
+    slNRF24_IoInit();
+    setupInt0();
+    slNRF24_Init();
+    status = 0;
+    slNRF24_GetRegister(CONFIG, &status, 1);
+    slUART_WriteString("sensor11 CONFIG: ");
+    slUART_LogBinaryNl(status);
+    slNRF24_RxPowerUp();
+    sei();
+    stage = 10;
+
     while (1) {
         switch (stage) {
-                break;
+            case 10:
+                goReset();
+            break;
             case 11:
                 waitForStart();
                 break;
@@ -90,43 +111,39 @@ void clearData() {
 }
 
 
-void nrf24_Start() {
-    slNRF_Init();
-    slNRF_FlushTX();
-    slNRF_OpenWritingPipe(pipe2, 9);
-    slNRF_OpenReadingPipe(pipe1, 9, 1);
-    slNRF_SetDataRate(RF24_250KBPS);
-    slNRF_SetPALevel(RF24_PA_MIN);
-    slNRF_SetChannel(77);
-    slNRF_DisableDynamicPayloads();
-    slNRF_EnableAckPayload();
-    slNRF_SetRetries(0, 3);
-    slNRF_AutoAck(1);
-    slNRF_PowerUp();
-    slNRF_StartListening();
-    clearData();
+void setupInt0() {
+    DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
+    // PD2 (PCINT0 pin) is now an input
+    PORTD |= (1 << PORTD2);    // turn On the Pull-up
+    // PD2 is now an input with pull-up enabled
+    EICRA |= (1 << ISC01);// INT0 falling edge PD2
+    EICRA &= ~(1 << ISC00);// INT0 falling edge PD2
+    EIMSK |= (1 << INT0);     // Turns on INT0
+    sei();
 }
 
 //stage 10
-
+void goReset(){
+    slUART_WriteStringNl("Sensor11 Reset");
+    slNRF24_Reset();
+    slNRF24_FlushTx();
+    slNRF24_FlushRx();
+    //wait for inerupt
+    stage = 0;
+}
 
 //stage 11
 void waitForStart() {
     counter = 0;
-    if (slNRF_Available()) {
-        slNRF_Recive(data, sizeof(data));
-        slUART_WriteStringNl("got data from server");
-        slUART_WriteStringNl((char *) data);
-        slNRF_StopListening();
-        slNRF_FlushTX();
-        slNRF_FlushRX();
-        slNRF_PowerDown();
-        stage = 12;
-    }
+    slUART_WriteStringNl("Sensor11 Go");
+    clearData();
+    //wait for inerupt
+    stage = 0;
 }
 
 //stage 12
 void compareStrings() {
+    slNRF24_GetRegister(R_RX_PAYLOAD,data,9);
     uint8_t go = 0;
     uint8_t i = sizeof(data);
     while (i--) {
@@ -134,22 +151,21 @@ void compareStrings() {
             go++;
         }
     }
-    //slUART_LogDecNl(go);
     if (go == sizeof(data)) {
-        slUART_WriteStringNl("got start command");
-        stage = 13;
+        slUART_WriteStringNl("Sensor11 got start command");
         clearData();
+        stage = 13;
     } else {
-        stage = 11;
+        stage = 0;//wait for interupt from got data
     }
 }
 
 //stage13
 uint8_t setBME280Mode() {
     counter = 0;
-    slUART_WriteStringNl("reset BME280");
+    slUART_WriteStringNl("Sensor11 reset BME280");
     if (BME280_SetMode(BME280_MODE_FORCED)) {
-        slUART_WriteString("Sensor set forced mode error!\r\n");
+        slUART_WriteString("Sensor11 Sensor set forced mode error!\r\n");
         return 1;
     }
     stage = 14;
@@ -162,7 +178,7 @@ uint8_t getDataFromBME280() {
     float temperature, humidity, pressure;
     if (BME280_ReadAll(&temperature, &pressure, &humidity)) {
 
-        slUART_WriteString("Sensor read error!\r\n");
+        slUART_WriteString("Sensor11 Sensor read error!\r\n");
         return 1;
     }
     BME180measure.temperature = calculateTemperature(temperature);
@@ -170,40 +186,47 @@ uint8_t getDataFromBME280() {
     BME180measure.pressure = calculatePressure(pressure);
     BME180measure.voltage = 323;
     BME180measure.sensorId = SENSOR_ID;
-    slUART_WriteStringNl("got data from BME280");
+    slUART_WriteStringNl("Sensor11 got data from BME280");
     slUART_LogDecNl(BME180measure.temperature);
     slUART_LogDecNl(BME180measure.humidity);
     slUART_LogDecNl(BME180measure.pressure);
     stage = 15;
+    sendingTryCount = 0;
     return 0;
 }
 
 //stage15
 uint8_t sendVianRF24L01() {
     counter = 0;
-    _delay_ms(4500);
     fillBuferFromMEASURE(BME180measure, data);
-    slUART_WriteStringNl("Sending data to server");
-    slNRF_PowerUp();
-    if (!slNRF_Sent(data, sizeof(data))) {
-        clearData();
-        counter = 0;
-        stage = 0;
-        slUART_WriteStringNl("Fail\n");
-        stage = 11;//waitForStart
-        slNRF_FlushTX();
-        slNRF_FlushRX();
-        slNRF_StartListening();
-        return 1;
-    } else {
-        slUART_WriteStringNl("SendOk\n");
-        counter = 0;
-        stage = 0;
-        clearData();
-        slNRF_FlushTX();
-        slNRF_FlushRX();
-        slNRF_StartListening();
-        stage = 11;//waitForStart
-    }
+    slUART_WriteStringNl("Sensor11 Sending data");
+    slNRF24_FlushTx();
+    slNRF24_FlushRx();
+    slNRF24_Reset();
+    slNRF24_TransmitPayload(&data, 9);
+    //_delay_ms(500);
+    // sendingTryCount = sendingTryCount + 1;
+    // if(sendingTryCount > 10){
+    //     stage = 10;//goReset
+    // }
+    stage = 0;
     return 0;
+}
+
+
+ISR(INT0_vect) {
+    status = 0;
+    slNRF24_GetRegister(STATUS, &status, 1);
+    slUART_WriteStringNl("Sensor11 STATUS:");
+    slUART_LogBinaryNl(status);
+    cli();
+    if ((status & (1 << 6)) != 0) {
+        slUART_WriteStringNl("Sensor11 Got Data");
+        stage = 12;//CompareStrings
+    }
+    if ((status & (1 << 5)) != 0) {
+        slUART_WriteStringNl("Sensor11 Sent Data");
+        stage = 10;//goReset
+    }
+    sei();
 }
