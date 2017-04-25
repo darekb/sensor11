9436c3d00c854e33011fe60cca4b435df3e2d1bd
darekb <kontakt@3wm.pl>
2017-04-25 18:14:07 +0200

    something works

--- main.c
+++ main.c
@@ -137,20 +137,21 @@ void setupInt0() {
 void goSleep(){
     // slUART_WriteStringNl("Sensor11 sleep");
     // _delay_ms(10);
-    inSleep = 1;
-    sei();
-    slNRF24_PowerDown();
-    stage = 0;
-    set_sleep_mode(SLEEP_MODE_IDLE);
-    sleep_enable();
-    power_adc_disable();
-    power_spi_disable();
-    power_timer0_disable();
-    power_timer2_disable();
-    power_twi_disable();
-    sleep_mode();
-    sleep_disable();
-    power_all_enable();
+    // inSleep = 1;
+    // sei();
+    // slNRF24_PowerDown();
+    // stage = 0;
+    // set_sleep_mode(SLEEP_MODE_IDLE);
+    // sleep_enable();
+    // power_adc_disable();
+    // power_spi_disable();
+    // power_timer0_disable();
+    // power_timer2_disable();
+    // power_twi_disable();
+    // sleep_mode();
+    // sleep_disable();
+    // power_all_enable();
+    goReset();
 }
 //stage 10
 void goReset(){
